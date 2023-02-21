a6c18f8a7b769ec37c75b3f33b027291d56d8cdf
vandaminator <ntokoanephomolo76@gmail.com>
2023-02-21 17:32:51 +0200

    added html and functionality

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-// code for searching
+// options is used with the fetch of each function
+
+const OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a9be9ca417msh02ffae03380bb1bp1b69a6jsn0f580a3248bd",
+    "X-RapidAPI-Host": "gogoanime2.p.rapidapi.com",
+  },
+};
+
+const TITLE = document.querySelector(".title");
+const IMAGE = document.querySelector(".picture");
+const SYNOPSIS = document.querySelector(".sypnosis");
 
 //get out data
 async function searchAnime(name) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a9be9ca417msh02ffae03380bb1bp1b69a6jsn0f580a3248bd",
-      "X-RapidAPI-Host": "gogoanime2.p.rapidapi.com",
-    },
-  };
   const response = await fetch(
     `https://gogoanime2.p.rapidapi.com/search?keyw=${name}`,
-    options
+    OPTIONS
   );
   return response.json();
 }
@@ -23,26 +28,97 @@ async function getAnimeName(name) {
     .catch((err) => console.error(err));
 }
 
+// returns only the infomation in json format
 async function searchAnimeDetails(name) {
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a9be9ca417msh02ffae03380bb1bp1b69a6jsn0f580a3248bd",
-      "X-RapidAPI-Host": "gogoanime2.p.rapidapi.com",
-    },
-  };
-
   const response = await fetch(
     `https://gogoanime2.p.rapidapi.com/anime-details/${name}`,
-    options
+    OPTIONS
   );
   return response.json();
 }
 
-async function getDetails(name, info) {
-  searchAnimeDetails(name)
-    .then((details) => console.log(details[info]))
-    .catch((err) => console.error(err));
+/* 
+
+searchAnimeDetails('overlord-iv') return example
+
+{
+  "animeTitle": "Overlord IV",
+  "type": "Summer 2022 Anime",
+  "releasedDate": "2022",
+  "status": "Completed",
+  "genres": [
+    "Action",
+    "Fantasy",
+    "Game",
+    "Magic",
+    "Supernatural"
+  ],
+  "otherNames": "オーバーロード IV",
+  "synopsis": "Fourth season of Overlord.",
+  "animeImg": "https://gogocdn.net/cover/overlord-iv.png",
+  "totalEpisodes": "13",
+  "episodesList": [
+    {
+      "episodeId": "overlord-iv-episode-3",
+      "episodeNum": "3",
+      "episodeUrl": "https://gogoanime.film//overlord-iv-episode-3"
+    },
+    {
+      "episodeId": "overlord-iv-episode-2",
+      "episodeNum": "2",
+      "episodeUrl": "https://gogoanime.film//overlord-iv-episode-2"
+    },
+    {
+      "episodeId": "overlord-iv-episode-1",
+      "episodeNum": "1",
+      "episodeUrl": "https://gogoanime.film//overlord-iv-episode-1"
+    }
+  ]
 }
 
-getDetails("blue-lock", `synopsis`);
+*/
+
+// using data from function here
+async function getDetails(name) {
+  searchAnimeDetails(name)
+    .then((details) => setInfo(details)) // load information on to screen
+    .catch((err) => setInfo(err));
+}
+
+// takes info in json format and loads it to screen
+async function setInfo(info) {
+  titleSet(info.animeTitle);
+  imageSet(info.animeImg);
+  synopsisSet(info.synopsis);
+}
+
+/* 
+the functions here make up setInfo function
+
+1. titleSet
+2. imageSet
+3. synopsisSet
+*/
+
+async function titleSet(title) {
+  TITLE.innerHTML = "";
+  const header = document.createElement("h1");
+  header.innerHTML = title;
+  TITLE.appendChild(header);
+}
+
+async function imageSet(src) {
+  IMAGE.innerHTML = "";
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = "anime";
+  img.width = 200;
+  IMAGE.appendChild(img);
+}
+
+async function synopsisSet(text) {
+  SYNOPSIS.innerHTML = "";
+  const synopsis = document.createElement("p");
+  synopsis.innerHTML = text;
+  SYNOPSIS.appendChild(synopsis);
+}
